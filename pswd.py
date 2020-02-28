20b0346af52e1e9b6a6593ce8b067bd811866534
Isaac Wu <shocker945516@gmail.com>
2020-02-28 10:59:27 +0800

    2nd commit

--- pswd.py
+++ pswd.py
@@ -1,13 +1,10 @@
 pswd = 'a123456'
 count = 3
-while True:
-	x = input('Please enter the password: ')
+while count > 0:
+	x = input('Enter password: ')
 	if x == pswd:
 		print('Login successfully!')
 		break
 	else:
 		count = count - 1
-		print('Incorrect password. You can try ', count, ' more time(s).')
-		if count == 0:
-			print('You can try up to 3 times!')
-			break
+		print('Incorrect password. You can try', count, 'more time(s).')
